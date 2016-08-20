3fef8023d63bd62de61648130c41862a81d74421
Oskar van Velden <oskar@rakso.nl>
2016-08-20 23:42:51 +0200

    Have random rotation and movement

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	public int maxAsteroids = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 Debug.DrawLine(Vector3.zero, new Vector3(1, 0, 0), Color.red);		 
+	}
+}
---
+++ Assets/Scripts/DebugBorder.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugBorder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dist = (transform.position - Camera.main.transform.position).z;
+
+		var leftBorder = Camera.main.ViewportToWorldPoint(
+		new Vector3(0, 0, dist)
+		).x;
+
+		var rightBorder = Camera.main.ViewportToWorldPoint(
+		new Vector3(1, 0, dist)
+		).x;
+
+		var topBorder = Camera.main.ViewportToWorldPoint(
+		new Vector3(0, 0, dist)
+		).y;
+
+		var bottomBorder = Camera.main.ViewportToWorldPoint(
+		new Vector3(0, 1, dist)
+		).y;
+
+		transform.position = new Vector3(
+		Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+		Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+		transform.position.z
+		);
+
+	}
+}
