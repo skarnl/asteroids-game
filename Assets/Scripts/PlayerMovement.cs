e691ed70321529e18c8adba2b03c06c023e2d7d7
Oskar van Velden <oskar@rakso.nl>
2016-08-20 23:19:28 +0200

    Rotate asteroid

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,21 +14,11 @@ public class PlayerMovement : MonoBehaviour {
 	void Awake() {
 		rb = GetComponent<Rigidbody2D>();
 	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical   = Input.GetAxis("Vertical");
-
-		Debug.Log(horizontal);
-		Debug.Log(horizontal * rotationSpeed * -1);
-		Debug.Log(horizontal * rotationSpeed);
-		Debug.Log("---");
 
 		transform.Rotate(Vector3.forward, horizontal * rotationSpeed * -1);
 		rb.AddRelativeForce(new Vector2(0, vertical * speed));
---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMovement : MonoBehaviour {
+
+	[Range(0, 30)]
+	public float speed = 5;
+
+	[Range(0, 20)]
+	public float rotationSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().AddTorque(Random.Range(-1f, 1f) * rotationSpeed, ForceMode2D.Force);
+		
+	}
+}
