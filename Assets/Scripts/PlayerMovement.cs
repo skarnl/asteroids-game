8d80413a096a11c5c3c163d9a7b47dd5b705d575
Oskar van Velden <oskar@rakso.nl>
2016-08-20 22:37:49 +0200

    Add PlayerMovement base script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	[Range(0, 30)]
+	public float speed = 2;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical   = Input.GetAxis("Vertical");
+
+		rb.AddForce(new Vector2(0, vertical * speed));
+	}
+}
