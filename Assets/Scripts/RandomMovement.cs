b3dda3737d410a6ee5f02c0d60d913da57086880
Oskar van Velden <oskar@rakso.nl>
2016-08-20 23:31:15 +0200

    Have random rotation and movement

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -6,12 +6,11 @@ public class RandomMovement : MonoBehaviour {
 	[Range(0, 30)]
 	public float speed = 5;
 
-	[Range(0, 20)]
-	public float rotationSpeed = 5;
-
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody2D>().AddTorque(Random.Range(-1f, 1f) * rotationSpeed, ForceMode2D.Force);
-		
+
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+		rb.AddForce(new Vector2(Random.Range(-1f, 1f) * speed, Random.Range(-1f, 1f) * speed));
 	}
 }
---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+	[Range(0, 20)]
+	public float rotationSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody2D>().AddTorque(Random.Range(-1f, 1f) * rotationSpeed, ForceMode2D.Force);
+	}
+}
