3bd8bf9d2c6dc6b1a5ee8103426569da00c51273
Oskar van Velden <oskar@rakso.nl>
2016-08-25 22:27:22 +0200

    Scrollbackground with transparancy

--- Assets/Scripts/ScrollBackground.cs
+++ Assets/Scripts/ScrollBackground.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class ScrollBackground : MonoBehaviour {
 
-	// Use this for initialization
+	public float scrollDevider = 2f;
+
+	private Material mat;
+
 	void Start () {
-	
+		mat = GetComponent<MeshRenderer>().material;
+
+		Color c = mat.color;
+		c.a = 0.1f;
+
+		mat.color = c;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		Vector2 materialOffset = mat.mainTextureOffset; 
+
+		materialOffset.x = transform.position.x / transform.localScale.x / scrollDevider;
+		materialOffset.y = transform.position.y / transform.localScale.y / scrollDevider;
+
+		mat.mainTextureOffset = materialOffset;
 	}
 }
