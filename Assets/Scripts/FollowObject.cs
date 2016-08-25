d600f384efb6da8df44c8eca17331398760826ae
Oskar van Velden <oskar@rakso.nl>
2016-08-25 22:10:53 +0200

    mAdd star-y background

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -3,33 +3,24 @@
  
  public class FollowObject : MonoBehaviour {
      
-     public float dampTime = 0.15f;
-     private Vector3 velocity = Vector3.zero;
      public Transform target;
 
      private Camera cam;
  
     void Start ()
     {
-        cam = GetComponent<Camera>();
-
-        if (target) {
-            //this is a little bullshit, but currently no idea (read: time) to make this correct
-		    Vector3 point = cam.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
-            Vector3 destination = transform.position + delta;
-            transform.position = destination;
-         }
+        Update();
      }
 
      // Update is called once per frame
      void Update () 
      {
          if (target) {
-			 Vector3 point = cam.WorldToViewportPoint(target.position);
-             Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
-             Vector3 destination = transform.position + delta;
-             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+             Vector3 targetPosition = target.position;
+             targetPosition.z = transform.position.z;
+
+             transform.position = targetPosition;
          }     
      }
  }
---
+++ Assets/Scripts/FollowWithCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ public class FollowWithCamera : MonoBehaviour {
+     
+     public float dampTime = 0.15f;
+     private Vector3 velocity = Vector3.zero;
+     public Transform target;
+
+     private Camera cam;
+ 
+    void Start ()
+    {
+        cam = GetComponent<Camera>();
+
+        if (target) {
+            //this is a little bullshit, but currently no idea (read: time) to make this correct
+		    Vector3 point = cam.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = destination;
+         }
+     }
+
+     // Update is called once per frame
+     void Update () 
+     {
+         if (target) {
+			 Vector3 point = cam.WorldToViewportPoint(target.position);
+             Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+             Vector3 destination = transform.position + delta;
+             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+         }     
+     }
+ }
