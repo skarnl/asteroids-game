ac1dc04119c591a95116a0a6dc2eab36ef6407a9
Oskar van Velden <oskar@rakso.nl>
2016-09-20 21:38:01 +0200

    Display score text

--- Assets/Scripts/Managers/PointManager.cs
+++ Assets/Scripts/Managers/PointManager.cs
@@ -3,11 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using Prime31.MessageKit;
+using UnityEngine.UI;
 
 public class PointManager : Singleton<PointManager> {
 	protected PointManager () {} // guarantee this will be always a singleton only - can't use the constructor!
  
     private int totalPoints;
+    private Text scoreText;
+
+    public void Awake () {
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+    }
 
 	public void AddPoints(int points) {
         totalPoints += points;
@@ -17,6 +23,6 @@ public class PointManager : Singleton<PointManager> {
 
     private void UpdateDisplay()
     {
-        print("##### Totalpoints: " + totalPoints);
+        scoreText.text = "Score: " + totalPoints;
     }
 }
