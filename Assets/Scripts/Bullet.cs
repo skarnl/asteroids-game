1857f05c6a9cf2c370472a6aa19b1a6283921e80
Oskar van Velden <oskar@rakso.nl>
2017-01-17 22:37:33 +0100

    Remove lowpoly shizzle, update ship render, add ship accelerate animation

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D coll) {
         Health selfHealth = GetComponent<Health>();
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
         if (selfHealth) {
             selfHealth.TakeHit();
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody2D rb;
+	Animator animator;
 
 	[Range(0, 5)]
 	public float speed = 2.5f;
@@ -13,12 +14,15 @@ public class PlayerMovement : MonoBehaviour {
 
 	void Awake() {
 		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical   = Input.GetAxis("Vertical");
+
+		animator.SetBool("accelerate", (vertical != 0));
 
 		transform.Rotate(Vector3.forward, horizontal * rotationSpeed * -1);
 		rb.AddRelativeForce(new Vector2(0, vertical * speed));
