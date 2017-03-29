fa836865d52ecc61fa952c12654c8b0ebfbb5658
Oskar van Velden <oskar@rakso.nl>
2017-03-29 21:11:33 +0200

    Enemy move to last know position

--- Assets/Scripts/_EnemyBehavior/MoveTowardsPlayer.cs
+++ Assets/Scripts/_EnemyBehavior/MoveTowardsPlayer.cs
@@ -8,6 +8,10 @@ public class MoveTowardsPlayer : MonoBehaviour {
     private GameObject player;
     public float speed = 5f;
 
+    private Vector3 goal;
+
+    public float RotationSpeed = 200f;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -16,7 +20,36 @@ public class MoveTowardsPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    float step = speed * Time.deltaTime;
-	    transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, step);
+
+	    if (goal != Vector3.zero) { // move towards last known position
+	        float step = speed * Time.deltaTime;
+	        transform.position = Vector3.MoveTowards(transform.position, goal, step);
+
+	        if (Vector2.Distance(transform.position, goal) < float.Epsilon) {
+	            goal = Vector3.zero;
+	        }
+	    }
+	    else { // look around
+	        transform.Rotate(Vector3.forward * (RotationSpeed * Time.deltaTime));
+	    }
 	}
+
+    void FixedUpdate()
+    {
+        float distance = 1.2f;
+
+        Debug.DrawRay(transform.position, transform.up * distance, Color.yellow);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, distance, 1 << LayerMask.NameToLayer("Player"));
+
+        if (hit.collider != null) {
+
+            print(hit.collider);
+            print(hit.collider.IsTouchingLayers(LayerMask.NameToLayer("Player")));
+
+            goal = hit.point;
+
+            print("HIT!");
+        }
+    }
 }
